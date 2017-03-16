6ef25193d60d7c541a1044b28889176bfaad54e2
Yulong Li <liyulong12@mails.ucas.ac.cn>
2017-03-16 15:05:46 +0800

    Update Pipeline

---
+++ RADassembler/Pipeline/bin/CP3_Opti.pl
@@ -0,0 +1,264 @@
+#!/usr/bin/env perl
+##	Author:Yu-Long Li
+use strict; 
+use warnings; 
+use Parallel::ForkManager;
+use Getopt::Long;
+
+
+my (@files, $file, $out_fh, $in_fh, $in_tags, $sub_lst);
+my $in_path;
+my $out_path;
+my $loci;
+my $num_threads = 1;
+my $cmd = '';	##Parameters parsed to CAP3 for the fist assembly.
+my $collect;
+my $help = 0;
+my $usage = 
+'	
+	Options [default values]:
+	
+	-i input path of fasta files
+	-o output path for assembly files
+	-l catalog file from stacks [batch_XXX.catalog.tags.tsv.gz]
+	-t number of threads [1]
+	-c collect the assembly results for the second reads [0]
+	-p parameters parsed to CAP3
+	-f sub file list for assembly 
+	-h help
+';
+
+GetOptions ("par=s" 		=> \$cmd,
+			"in=s" 			=> \$in_path,
+			"out=s" 		=> \$out_path,
+			"loci=s" 		=> \$loci,
+			"threads=i" 	=> \$num_threads,
+			"collect:1" 	=> \$collect,
+			"file:s" 	    => \$sub_lst,
+			"help:1"		=> \$help
+			)
+			or die ("Error in command line arguments\n");
+	
+die "$usage\n" if !($in_path && $out_path && $loci) or ($help);
+
+## If the outpath does not exist, create it;
+
+if (! -e "$out_path/assembly_1st") {
+	
+	`mkdir -p $out_path/assembly_1st`;
+	`mkdir -p $out_path/log`
+	
+}
+
+##delete any tmp files;
+
+`find $in_path/ -name "*cap*" |xargs rm -rf`;
+
+
+print "Starting the first run...\n";
+unless($cmd) {
+	#Parameters for short reads.
+	$cmd = '-r 0 -i 30 -j 31 -o 18 -s 300 -p 85';
+}
+
+`echo -e "cap3 $cmd" > $out_path/log/assembly.1par`;
+
+
+##	extract consensus from stacks catalogs;
+
+if (substr($loci, -2) eq 'gz') {
+	open($in_tags, "gunzip -c $loci|") or die "$!";
+	} 
+elsif (substr($loci, -2) eq '7z') {
+	open($in_tags, "7zcat.sh $loci|") or die "$!";
+	}
+else {
+	open($in_tags, "$loci") or die "$!";
+}
+my %seqs;
+while (<$in_tags>) {
+	
+	next if /^#/;
+	my @parts = split(/\t/, $_);
+	my $id    = $parts[2];
+	my $seq   = $parts[9];
+	die "catalog file is wrong!!\n" if ($id+1 ne $.); 
+	$seq = uc(reverse $seq);
+	$seq =~ tr/ATCGN/TAGCN/;
+	$seqs{$id} = $seq;
+		
+}
+
+close $in_tags;
+
+## if provided sub file list...
+if ($sub_lst) {
+	open(my $in_fh, "$sub_lst") or die "$!";
+	while(<$in_fh>) {
+		next if /^$/;
+		chomp;
+		push @files, $_;
+	}
+	close $in_fh;
+} else {
+	get_flist($in_path,$out_path,"reads_1st","fa");
+}
+
+##	multi-threading part for the 1st assembly;
+my $thread_m = new Parallel::ForkManager($num_threads);
+my $total = @files;
+my $i;
+foreach $file (@files){
+	
+	$i++;
+	$thread_m->start and next;
+	
+	run_assembly($file, $in_path, "$out_path/assembly_1st", 1, $cmd);
+	print STDERR "Assembling locus $i; run $i of $total        \r";
+	
+	$thread_m->finish;
+	
+	
+}
+
+$thread_m->wait_all_children;
+`find $in_path/ -name "*cap*" |xargs rm -rf`;
+
+## 2nd assembly;
+print "\nStarting the second run...\n";
+#######
+
+`echo -e "cap3 $cmd" > $out_path/log/assembly.2par`;
+#######
+
+if (! -e "$out_path/assembly_2nd") {
+	
+	`mkdir -p $out_path/assembly_2nd`;
+	
+}
+
+
+get_flist("$out_path/assembly_1st/", $out_path, "reads_2nd", "fa");
+
+foreach $file (@files){
+	
+	
+	
+	#print "start one threads" . "\n";
+
+	$thread_m->start and next;
+	
+	run_assembly($file, "$out_path/assembly_1st", "$out_path/assembly_2nd", 2, $cmd);
+	
+	$thread_m->finish;
+	
+	
+}
+
+$thread_m->wait_all_children;
+`find $out_path/assembly_1st/ -name "*cap*" |xargs rm -rf`;
+
+##collect the final results, and delet useless files;
+print "Starting to collect the final contigs...\n";
+
+get_flist("$out_path/assembly_2nd", $out_path, "2nd_assembled","fa");	# get the list of second assembly files;
+
+parse_fasta(\@files, "$out_path/assembly_2nd", "$out_path", "collected_final.fa");
+
+
+
+##	if collect contigs for reads1;
+if ($collect) {
+	
+	my $in_fa = $out_path . "/" . "assembly_1st";
+	my $out_fa = $out_path;
+	my $name_fa = "collected_reads1.fa";
+	
+	
+	get_flist("$out_path/assembly_1st",$out_path,"collect_reads1","fa");
+	parse_fasta(\@files, $in_fa, $out_fa, $name_fa);
+	
+	`sed -i /Consensus/d  $out_fa/$name_fa`;
+	
+}
+
+
+sub parse_fasta {
+
+	my ($files, $in_fa, $out_fa, $name_fa) =@_;
+	my ($in_fh, $out_fh, $file);
+	
+	open ($out_fh, ">$out_fa/$name_fa") or die "$!";
+	
+	foreach $file (@files) {
+		open ($in_fh, "<$in_fa/$file") or die "$!";
+		while (<$in_fh>) {
+		
+		chomp if /Consensus/;
+		$_ =~ s/^>/>$file\./;
+		print $out_fh $_;
+		
+		}
+		
+		close $in_fh;
+	}
+	
+	#print $out_fh "\n";	#the last line;
+	close $out_fh;
+	
+	
+}
+
+sub run_assembly {
+
+	
+	my ($file, $in_path, $out_path, $run, $cmd) = @_;
+	
+	`cap3 $in_path/$file $cmd 1>/dev/null 2>>$out_path/error.log`; #Changed, maybe some bugs here.
+	unless (-z "$in_path/$file.cap.contigs") {
+		
+		`mv $in_path/$file.cap.contigs $out_path/$file`;
+		
+	}
+	`rm -rf $in_path/$file.cap.*`;
+	
+	
+	if ($run ==1) {
+		my $locus = substr($file, 0, -3);
+		my $seq = $seqs{$locus};
+		open (my $out_fh, ">>$out_path/$file");
+		print $out_fh 
+		">" . $locus . "_Consensus" . "\n" . $seq . "\n";
+		
+		close $out_fh;
+	}
+	
+	
+	
+	
+	
+}
+
+
+sub get_flist {
+	my ($in_path, $out_path, $name, $type) = @_;
+	
+	`mkdir -p $out_path/log/`;
+	
+	
+	undef (@files);
+	opendir (D, $in_path) or die "$!";
+	open(my $out_list, ">$out_path/log/$name\.txt") or die "$!";
+
+	##	get the file list;
+	
+	while (($file = readdir(D))) {
+		next if $file !~ /.+\.$type$/;
+		print $out_list $file . "\n";
+		push (@files, $file);
+	}
+	
+	return @files;
+	close $out_list;
+	close D;
+}
---
+++ RADassembler/Pipeline/bin/sort_read_pairs.pl
@@ -0,0 +1,515 @@
+#!/usr/bin/env perl
+#
+# Copyright 2011-2015, Julian Catchen <jcatchen@illinois.edu>
+#
+# This file is part of Stacks.
+#
+# Stacks is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Stacks is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Stacks.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+#
+# Sort paired-end sequences according to the stacks the non-paired-end 
+# was found in. 
+#
+# By Julian Catchen <jcatchen@illinois.edu>
+#
+
+use strict;
+use constant stacks_version => "1.32";
+
+use constant true  => 1;
+use constant false => 0;
+
+my $debug          = 0;
+my $white_list     = "";
+my $cat_white_list = "";
+my $in_path        = "";
+my $out_path       = "";
+my $samp_path      = "";
+my $out_type       = "fasta";
+my $in_type        = "fastq";
+my $gzipped        = false;
+
+parse_command_line();
+
+my (@files, %matches, %stacks, %reads, %marker_wl);
+
+build_file_list(\@files);
+
+my ($file, $num_files, $i, $key);
+
+if (length($cat_white_list) > 0) {
+    load_white_list($cat_white_list, \%marker_wl);
+    print STDERR "Loaded ", scalar(keys %marker_wl), " catalog IDs from '$cat_white_list'\n";
+}
+
+$num_files = scalar(@files);
+$i         = 1;
+foreach $file (@files) {
+    printf(STDERR "Loading catalog matches, file % 2s of % 2s [%s]\n", $i, $num_files, $file->{'prefix'});
+    #
+    # Load the sstacks file, listing matches between the stacks and the catalog
+    #
+    load_matches($in_path, $file, \%matches, \%marker_wl);
+
+    $i++;
+}
+
+#
+# Determine which catalog loci have more than a single match from each sample and blacklist them.
+#
+print STDERR "Identifying catalog loci that have more than one match from a single sample...";
+my %multiple_matches;
+check_mult_catalog_matches(\%matches, \%multiple_matches);
+print STDERR 
+    "done\n",
+    "  These loci will be excluded when collating paired-end reads;\n",
+    "  A list of them has been recorded: $out_path/sort_read_pairs.log\n",
+    scalar(keys %matches), " total catalog loci; ", scalar(keys %multiple_matches), 
+    " will be excluded, processing ", scalar(keys %matches) - scalar(keys %multiple_matches), " loci.\n";
+
+#
+# Check if files already exist, if so, exit to prevent adding data to existing files.
+#
+my ($cat_id, $path);
+
+foreach $cat_id (keys %matches) {
+    #
+    # Check that this catalog ID only has a single match from each sample.
+    #
+    next if (defined($multiple_matches{$cat_id}));
+
+    $path  = $out_path . "/" . $cat_id;
+    $path .= $out_type eq "fasta" ? ".fa" : ".fq";
+
+    if (-e $path) {
+	die("Error: output files already exist. This program will append data to files if\n" .
+	    "they already exist. Please delete these files and re-execute sort_read_pairs.pl.\n");
+    }
+}
+
+$i = 1;
+foreach $file (@files) {
+    printf(STDERR "Processing file % 2s of % 2s [%s]\n", $i, $num_files, $file->{'prefix'});
+    #
+    # Load the ustacks tag file for each sample, containing the read-to-stack mappings
+    #
+    $stacks{$file->{'prefix'}} = {};
+
+    print STDERR "  Loading tag file...";
+    load_stacks($in_path, $file, $stacks{$file->{'prefix'}});
+    print STDERR "done.\n";
+
+    #
+    # Map the read-pairs to the stack/catalog match they correspond to.
+    #
+    $reads{$file->{'prefix'}} = {};
+
+    print STDERR "  Loading sample file...";
+    $in_type eq "fastq" ?
+	process_fastq_read_pairs($samp_path, $file, \%stacks, $reads{$file->{'prefix'}}) :
+	process_fasta_read_pairs($samp_path, $file, \%stacks, $reads{$file->{'prefix'}});
+    print STDERR "done.\n";
+
+    print STDERR "  Printing results...";
+    print_results($out_path, \%matches, \%stacks, \%reads, \%multiple_matches);
+    print STDERR "done.\n";
+
+    #
+    # Clean up memory usage.
+    #
+    print STDERR "  Clearing memory...";
+    undef(%{$stacks{$file->{'prefix'}}});
+    undef(%{$reads{$file->{'prefix'}}});
+    print STDERR "  done.\n";
+
+    $i++;
+}
+
+sub load_matches {
+    my ($in_path, $in_file, $matches, $marker_wl) = @_;
+
+    my ($file, $in_fh, $line, @parts, $key);
+
+    if ($gzipped == true) {
+	$file  = $in_path . "/" . $in_file->{'prefix'} . $in_file->{'suffix'} . ".matches.tsv.gz";
+	open($in_fh, "gunzip -c $file |") or die("Unable to open catalog matches file '$file', $!\n");
+    } else {
+	$file  = $in_path . "/" . $in_file->{'prefix'} . $in_file->{'suffix'} . ".matches.tsv";
+	open($in_fh, "<$file") or die("Unable to open catalog matches file '$file', $!\n");
+    }
+
+    while ($line = <$in_fh>) {
+        chomp $line;
+        @parts = split(/\t/, $line);
+
+        if (length($cat_white_list) > 0) {
+	    next if (!defined($marker_wl->{$parts[2]}));
+	}
+
+        if (!defined($matches->{$parts[2]})) {
+            $matches->{$parts[2]} = {};
+        }
+
+        #
+        # Index by catalog_ID -> sample_ID|stack_ID
+        #
+        $key = $in_file->{'prefix'} . "|" . $parts[4];
+        $matches->{$parts[2]}->{$key}++;
+    }
+
+    close($in_fh);
+}
+
+sub load_stacks {
+    my ($in_path, $in_file, $stacks) = @_;
+
+    my ($file, $in_fh, $line, @parts);
+
+    if ($gzipped == true) {
+	$file = $in_path . "/" . $in_file->{'prefix'} . $in_file->{'suffix'} . ".tags.tsv.gz";
+	open($in_fh, "gunzip -c $file |") or die("Unable to open '$file', $!\n");
+    } else {
+	$file = $in_path . "/" . $in_file->{'prefix'} . $in_file->{'suffix'} . ".tags.tsv";
+	open($in_fh, "<$file") or die("Unable to open '$file', $!\n");
+    }
+
+    while ($line = <$in_fh>) {
+        chomp $line;
+        @parts = split(/\t/, $line);
+
+        next if ($parts[6] eq "consensus" || $parts[6] eq "model");
+
+        #
+        # Index by sequence ID -> stack ID
+        #
+        $stacks->{(split(/\s+/,$parts[8],2))[0]} = $parts[2];	#Modified select the second field of fastq header 
+    }
+
+    close($in_fh);
+}
+
+sub process_fastq_read_pairs {
+    my ($in_path, $in_file, $stacks, $reads) = @_;
+
+    my ($file, $in_fh, $line, $seq, $qual, $key, $read_id, $read_dd);
+
+	if ($gzipped == true) {
+	    $file = $in_path . "/" . $in_file->{'prefix'} . "_1.fq.gz";
+	    open($in_fh, "gunzip -c $file |") or ($file = $in_path . "/" . $in_file->{'prefix'} . "_2.fq.gz");
+		open($in_fh, "gunzip -c $file |") or die("Unable to open paired-end input file '$file'\n");
+	} else {
+	    $file = $in_path . "/" . $in_file->{'prefix'} . "_1.fq";
+	    open($in_fh, "<$file") or ($file = $in_path . "/" . $in_file->{'prefix'} . "_2.fq");
+		open($in_fh, "<$file") or die("Unable to open paired-end input file '$file'\n");
+	}
+    
+
+    while ($line = <$in_fh>) {
+	next if (substr($line, 0, 1) ne "@");
+	chomp $line;
+
+    $read_id = substr((split(/\s+/,$line))[0], 1);	#Modified
+	$read_dd = substr($line, 1); #Original read id.
+	$seq     = <$in_fh>;
+	chomp $seq;
+
+	#
+	# Read the repeated ID and the quality scores.
+	#
+	<$in_fh>;
+	$qual = <$in_fh>;
+	chomp $qual;
+
+        $key = $stacks->{$in_file->{'prefix'}}->{$read_id};
+
+        next if (!defined($key));
+
+        if (!defined($reads->{$key})) {
+            $reads->{$key} = [];
+        }
+
+        push(@{$reads->{$key}}, {'id' => $read_dd, 'seq' => $seq, 'qual' => $qual}); #
+    }
+}
+
+sub process_fasta_read_pairs {
+    my ($in_path, $in_file, $stacks, $reads) = @_;
+
+    my ($file, $in_fh, $line, $seq, $qual, $key, $read_id, $read_dd);
+
+	if ($gzipped == true) {
+		$file = $in_path . "/" . $in_file->{'prefix'} . "_1.fa.gz";
+	    open($in_fh, "gunzip -c $file |") or $file = $in_path . "/" . $in_file->{'prefix'} . "_2.fa.gz";
+		open($in_fh, "gunzip -c $file |") or die("Unable to open paired-end input file '$file'\n");
+	} else {
+	    $file = $in_path . "/" . $in_file->{'prefix'} . "_1.fa";
+	    open($in_fh, "<$file") or $file = $in_path . "/" . $in_file->{'prefix'} . "_2.fa";
+		open($in_fh, "<$file") or die("Unable to open paired-end input file '$file'\n");
+	}
+    while ($line = <$in_fh>) {
+	next if (substr($line, 0, 1) ne ">");
+	chomp $line;
+	
+    $read_id = substr((split(/\s+/,$line))[0], 1);	#Modified
+	$read_dd = substr($line, 1); #Original read id.
+	#print $read_id;
+	$seq     = <$in_fh>;
+	chomp $seq;
+
+        $key = $stacks->{$in_file->{'prefix'}}->{$read_id};
+
+        next if (!defined($key));
+
+        if (!defined($reads->{$key})) {
+            $reads->{$key} = [];
+        }
+
+        push(@{$reads->{$key}}, {'id' => $read_dd, 'seq' => $seq, 'qual' => ""}); #
+    }
+}
+
+sub print_results {
+    my ($out_path, $matches, $stacks, $reads, $multiple_matches) = @_;
+
+    my ($path, $cat_id, $sample, $stack_id, $read, $out_fh, $i, @keys, $count, $key, $mult_hits);
+
+    # 
+    # If a catalog ID matches stacks from multiple samples, print them out together.
+    #
+    foreach $cat_id (keys %{$matches}) {
+        #
+        # Check that this catalog ID only has a single match from each sample.
+        #
+        next if (defined($multiple_matches->{$cat_id}));
+
+        $path  = $out_path . "/" . $cat_id;
+	$path .= $out_type eq "fasta" ? ".fa" : ".fq";
+        open($out_fh, ">>$path") or die("Unable to open $path; '$!'\n");
+
+        foreach $key (keys %{$matches->{$cat_id}}) {
+
+            ($sample, $stack_id) = split(/\|/, $key);
+
+            foreach $read (@{$reads->{$sample}->{$stack_id}}) {
+		if ($out_type eq "fasta") {
+		    print $out_fh
+			">", $cat_id, "|", $sample, "|", $stack_id, "|", $read->{'id'}, "\n",
+			$read->{'seq'}, "\n";
+		} else {
+		    print $out_fh
+			"@", $cat_id, "|", $sample, "|", $stack_id, "|", $read->{'id'}, "\n",
+			$read->{'seq'}, "\n",
+			"+\n",
+			$read->{'qual'}, "\n";
+		}
+            }
+        }
+
+        close($out_fh);
+    }
+}
+
+sub check_mult_catalog_matches {
+    my ($matches, $multiple_matches) = @_;
+
+    my ($fh, $key, $sample, $stack_id);
+
+    #
+    # Find catalog loci that have more than a single match from one or more samples
+    # and log those loci that will be excluded.
+    #
+    open($fh, ">$out_path/sort_read_pairs.log") or die("Unable to open log file, $!\n");
+    print $fh 
+	"# The catalog loci listed below have more than a single match from one or more individuals, indicating undermerged or repetitive loci.\n",
+	"# CatalogLocus <tab> Sample1:Locus1,Locus2;Sample2:Locus1,Locus2\n";
+
+    foreach $cat_id (keys %{$matches}) {
+	my %samples;
+
+	foreach $key (keys %{$matches->{$cat_id}}) {
+	    ($sample, $stack_id) = split(/\|/, $key);
+	    push(@{$samples{$sample}}, $stack_id);
+	}
+
+	my $mult_hits = 0;
+	my $str = "";
+
+	foreach $sample (keys %samples) {
+	    if (scalar(@{$samples{$sample}}) > 1) {
+		$mult_hits++;
+		$str .= $sample . ":" . join(",", @{$samples{$sample}}) . "; ";
+	    }
+	}
+    
+	if ($mult_hits > 0) {
+	    print $fh $cat_id, "\t", substr($str, 0, -1), "\n";
+	    $multiple_matches{$cat_id}++;
+	}
+    }
+
+    close($fh);
+}
+
+sub count_reads {
+    my ($catalog, $reads) = @_;
+
+    my ($count, $key, $sample, $stack_id);
+
+    $count = 0;
+
+    foreach $key (keys %{$catalog}) {
+        ($sample, $stack_id) = split(/\|/, $key);
+
+	if (defined($reads->{$sample}->{$stack_id})) {
+	    $count += scalar(@{$reads->{$sample}->{$stack_id}});
+	}
+    }
+
+    return $count;
+}
+
+sub build_file_list {
+    my ($files) = @_;
+
+    my (@ls, $line, $file, $prefix, $suffix);
+
+    # Load a white list of files to process if it is supplied.
+    my %wl;
+    if (length($white_list) > 0) {
+	load_white_list($white_list, \%wl);
+	print STDERR "Loaded ", scalar(keys %wl), " filenames from '$white_list'\n";
+    }
+
+    @ls = glob("$in_path/*.tags.tsv");
+    if (scalar @ls == 0) {
+	@ls = glob("$in_path/*.tags.tsv.gz");
+	$gzipped = true if (scalar @ls > 0);
+    }
+
+    foreach $line (@ls) {
+	chomp $line;
+
+	next if (length($line) == 0);	
+	next if ($line =~ /batch_\d+\.catalog/);
+
+	($file) = ($line =~ /$in_path\/(.+)\.tags\.tsv\.?g?z?$/); 
+
+        if (length($white_list) > 0) {
+	    next if (!defined($wl{$file}));
+	}
+
+	if ($file =~ /\_1$/) {
+	    ($prefix, $suffix) = ($file =~ /^(.+)(\_1)$/);
+	} else {
+	    $prefix = $file;
+	    $suffix = "";
+	}
+
+	push(@{$files}, {'prefix' => $prefix, 'suffix' => $suffix});
+    }
+}
+
+sub load_white_list {
+    my ($list, $wl) = @_;
+
+    open(WHITE, "<" . $list) 
+	or die("Unable to open white list file '$white_list': $!\n");
+
+    my $line   = "";
+
+    while ($line = <WHITE>) {
+	chomp $line;
+
+	next if (length($line) == 0);
+	next if ($line =~ /^\s*#/);
+
+	$wl->{$line}++;
+    }
+
+    close(WHITE);
+}
+
+sub parse_command_line {
+    while (@ARGV) {
+	$_ = shift @ARGV;
+	if    ($_ =~ /^-p$/) { $in_path    = shift @ARGV; }
+	elsif ($_ =~ /^-o$/) { $out_path   = shift @ARGV; }
+	elsif ($_ =~ /^-s$/) { $samp_path  = shift @ARGV; }
+	elsif ($_ =~ /^-t$/) { $out_type   = shift @ARGV; }
+	elsif ($_ =~ /^-i$/) { $in_type    = shift @ARGV; }
+	elsif ($_ =~ /^-W$/) { $white_list = shift @ARGV; }
+	elsif ($_ =~ /^-w$/) { $cat_white_list = shift @ARGV; }
+	elsif ($_ =~ /^-d$/) { $debug++; }
+	elsif ($_ =~ /^-v$/) { version(); exit(); }
+	elsif ($_ =~ /^-h$/) { usage(); }
+	else {
+	    print STDERR "Unknown command line option: '$_'\n";
+	    usage();
+	}
+    }
+
+    if ($out_type ne "fasta" && $out_type ne "fastq") {
+	print STDERR "Output type must be either 'fasta' or 'fastq'.\n";
+	usage();
+    }
+
+    if ($in_type ne "fasta" && $in_type ne "fastq") {
+	print STDERR "Input type must be either 'fasta' or 'fastq'.\n";
+	usage();
+    }
+
+    if (length($in_path) == 0) {
+	print STDERR "You must specify a path to the Stacks output files.\n";
+	usage();
+    }
+
+    if (length($out_path) == 0) {
+	print STDERR "You must specify a path to write the collated output files.\n";
+	usage();
+    }
+
+    if (length($samp_path) == 0) {
+	print STDERR "You must specify a path to the paired-end reads.\n";
+	usage();
+    }
+
+    $in_path   = substr($in_path, 0, -1)   if (substr($in_path, -1)   eq "/");
+    $out_path  = substr($out_path, 0, -1)  if (substr($out_path, -1)  eq "/");
+    $samp_path = substr($samp_path, 0, -1) if (substr($samp_path, -1) eq "/");
+}
+
+sub version {
+    print STDERR "sort_read_pairs.pl ", stacks_version, "\n";
+}
+
+sub usage {
+    version();
+
+    print STDERR <<EOQ; 
+sort_read_pairs.pl -p path -s path -o path [-t type] [-W white_list] [-w white_list] [-d] [-h]
+    p: path to the Stacks output files.
+    s: path to paired-end sample files.
+    o: path to output the collated FASTA files.
+    i: input type, either 'fasta' or 'fastq' (default).
+    t: output type, either 'fasta' (default) or 'fastq'.
+    W: a white list of files to process in the input path.
+    w: a white list of catalog IDs to include.
+    h: display this help message.
+    d: turn on debug output.
+
+EOQ
+
+exit(0);
+}
